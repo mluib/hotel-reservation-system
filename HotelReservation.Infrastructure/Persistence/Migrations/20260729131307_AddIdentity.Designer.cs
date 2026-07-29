d839a69508c1385166f671b559a7959b2eb018ce
Manuel <manuel@example.com>
2026-07-29 15:15:48 +0200

    Add Identity database migration

--- HotelReservation.Infrastructure/Persistence/Migrations/20260729131307_AddIdentity.Designer.cs
+++ HotelReservation.Infrastructure/Persistence/Migrations/20260729131307_AddIdentity.Designer.cs
@@ -4,6 +4,7 @@ using HotelReservation.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelReservation.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(HotelDbContext))]
-    partial class HotelDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260729131307_AddIdentity")]
+    partial class AddIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
